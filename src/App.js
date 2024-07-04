e53978a8073a7a53bcc235e273b50689fb9c8fd6
Roman Piacquadio <romanpiacquadio@hotmail.com>
2024-07-03 21:17:37 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const authToken = 'pk_43781638_97MH4EGOJRHDXLB3CBMPF8G95D61XXWD';
+  
+  const [lists, setLists] = useState([]);
+  const [tasks, setTasks] = useState([]);
+
+  const fetchLists = async () => {
+    try {
+      const { data } = await axios.get(`https://api.clickup.com/api/v2/space/90140484102/list`,
+        {
+          headers: {
+            Authorization: authToken,
+          },
+        }
+      );
+      console.log( data.lists );
+      setLists( data.lists );
+    } catch ( error ) {
+      console.log('Error Fetching Lists', error);
+    }
+  };
+
+  const fetchTasks = async ( id ) => {
+    try {
+      const { data } = await axios.get(`https://api.clickup.com/api/v2/list/${ id }/task`,
+        {
+          headers: {
+            Authorization: authToken,
+          },
+        }
+      );
+      console.log( data );
+      // setTasks();
+    } catch ( error ) {
+      console.log('Error Fetching Tasks', error)
+    }
+  }
+
+  useEffect(() => {
+    fetchLists();
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
